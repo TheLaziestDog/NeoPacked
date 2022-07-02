890da8532916c8a89be2667e3284354bbc5c465b
LazyDog <108339770+TheLaziestDog@users.noreply.github.com>
2022-07-02 14:10:25 +0700

    Adding minor changes

--- lua/retro/init.lua
+++ lua/retro/init.lua
@@ -1,6 +1,6 @@
-" --- | INIT.LUA | ---
+--- | INIT.LUA | ---
 
-" This is the file where you initialize all of your lua stuff (plugins, etc)
+-- This is the file where you initialize all of your lua stuff (plugins, etc)
 
 require('retro.vui')
 require('retro.vux')
