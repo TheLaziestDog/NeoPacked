c1839644ff536aa2ac9b4d2c5969c41178fca075
LazyDog <108339770+TheLaziestDog@users.noreply.github.com>
2022-07-02 19:22:03 +0700

    Adding slight changes

--- lua/retro/vux.lua
+++ lua/retro/vux.lua
@@ -21,4 +21,45 @@ end
 remap('i' , '<CR>','v:lua.MUtils.completion_confirm()', {expr = true , noremap = true})
 
 -- Telescope
-require('telescope').setup{}
+require('telescope').setup{
+  extensions = {
+    command_palette = {
+      {"File",
+        { "entire selection", ':call feedkeys("GVgg")' },
+        { "save current file", ':w' },
+        { "save all filess)", ':wa' },
+        { "quit", ':qa' },
+        { "file browser", ":lua require'telescope'.extensions.file_browser.file_browser()", 1 },
+        { "git files", ":lua require('telescope.builtin').git_files()", 1 },
+        { "files",     ":lua require('telescope.builtin').find_files()", 1 },
+      },
+      {"Help",
+        { "tips", ":help tips" },
+        { "cheatsheet", ":help index" },
+        { "tutorial", ":help tutor" },
+        { "summary", ":help summary" },
+        { "quick reference", ":help quickref" },
+        { "search help", ":lua require('telescope.builtin').help_tags()", 1 },
+      },
+      {"Vim",
+        { "reload vimrc", ":source $MYVIMRC" },
+        { 'check health', ":checkhealth" },
+        { "jumps", ":lua require('telescope.builtin').jumplist()" },
+        { "commands", ":lua require('telescope.builtin').commands()" },
+        { "command history", ":lua require('telescope.builtin').command_history()" },
+        { "registers", ":lua require('telescope.builtin').registers()" },
+        { "colorshceme", ":lua require('telescope.builtin').colorscheme()", 1 },
+        { "vim options", ":lua require('telescope.builtin').vim_options()" },
+        { "keymaps", ":lua require('telescope.builtin').keymaps()" },
+        { "buffers", ":Telescope buffers" },
+        { "search history", ":lua require('telescope.builtin').search_history()" },
+        { "paste mode", ':set paste!' },
+        { 'cursor line', ':set cursorline!' },
+        { 'cursor column', ':set cursorcolumn!' },
+        { "spell checker", ':set spell!' },
+        { "relative number", ':set relativenumber!' },
+        { "search highlighting", ':set hlsearch!' },
+      }
+    }
+  }}
+
